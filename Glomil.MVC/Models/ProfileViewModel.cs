7c65e07a4d81a6e551e351014ca6399bb13d3d42
Bahadir Cengel <cengel.bahadir@outlook.com>
2021-05-30 23:38:23 +0300

    View Model added

---
+++ Glomil.MVC/Models/ProfileViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Glomil.MVC.Models
+{
+    public class ProfileViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Nickname { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
